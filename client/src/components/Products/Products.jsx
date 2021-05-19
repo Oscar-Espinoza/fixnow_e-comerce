7d0ed51079bec6027159173f24682a4c351778d6
unknown <oscardavid2094@gmail.com>
2021-05-19 18:31:10 -0300

    algo

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,37 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import { Grid } from '@material-ui/core'
+import { useEffect } from 'react';
+import { Grid } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../../redux/actions/productActions';
 
 import useStyles from './styles'
 
 import Product from './Product/Product'
 
-// const products = [
-
-//     {id: 1, name: 'iPhone 7 screen', description: 'Modulo de iPhone 7', price: '$1000', img: 'https://www.fixshop.eu/media/carousel_images/1100013305-099_4854737483904091303.jpg.500x0_q85_crop_replace_alpha-%23fff.jpg'},
-
-//     {id: 2, name: 'iPhone 8 screen', description: 'Modulo de iPhone 8', price: '$1500', img: 'https://www.fixshop.eu/media/carousel_images/1100018646-099_1054087305388501952.jpg.500x0_q85_crop_replace_alpha-%23fff.jpg'}
-
-// ]
 
 const Products = () => {
-
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch();
     const classes = useStyles()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        let mounted = true
-        const fetchData = async () => {
-        
-        const { data } = await axios.get('/api/products');
-        setProducts(data);       
-        };
-        fetchData()
-
-        return () => {
-            mounted = false;
-        }
-    }, [])
+        dispatch(listProducts())
+        console.log(products)
+    }, []);
     return( 
         <main className={classes.content}>
             <div className={classes.toolbar} />
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,11 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
-import data from '../data';
+import { productListReducer } from './reducers/productReducer'
 
 const initialState = {};
-const reducer = (state, action) => {
-    return { products: data.products}
-};
+const reducer = combineReducers({
+    productList: productListReducer
+})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
